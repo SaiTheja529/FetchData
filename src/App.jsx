0f372446603b66154f252801098301f3100e035b
SaiTheja529 <p.sivasaitheja@gmail.com>
2025-01-09 09:57:23 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       })
       .catch((error) => console.error("Error fetching data:", error));
 
-  },)
+  },[posts])
 
 
   return (
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,26 @@
-import React from 'react' 
+import React from 'react'
 import '../bootstrap-5.3.3-dist/css/bootstrap.css'
-const Card=({props})=>{
-    
-    return(
+const Card = ({ post}) => {
+
+    return (
 
         <>
-          <div className="card" style={{width:"18rem"}}>
+            <div className="d-flex justify-content-center align-items-center">
+                <div className="row d-md-flex  d-sm-flex justify-content-md-center  justify-content-sm-center align-items-md-center align-items-sm-center">
 
-            <div className="card-body">
-                <h5 className="card-title"></h5>
-                <p className="card-text">{props.text}</p>
-                <a href="#" className="btn btn-primary">Go SomeWhere</a>
+                    {posts.map((post) => (
+                        <div className="col-md-4 m-4 col-sm-12 " >
+
+                            <div className="card h-100  text-bg-warning border-dark">
+                                <div className="card-body">
+                                    <h5 className="card-title text-primary">{post.title}</h5>
+                                    <p className="card-text text-success">{post.body}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-        
         </>
     )
 
